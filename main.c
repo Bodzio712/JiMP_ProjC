279493785b4fc8353b2792949d1344300924496e
Damian <frajdad@jimp>
2017-03-27 22:54:11 +0200

    wrzucam podstawe main i strukture

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main (int argc, char ** argv) {
-	printf("%s\n", argv[0]);
-	return 0;
+#include "struktura.h"
+
+int main(int argc, char **argv) {
+
+    FILE *config = fopen(argv[1], "r");
+    FILE *wspolrzedne = fopen(argv[2], "r");
+    FILE *wynik = fopen(argv[3], "w");
+    int sas, wys, szer;
+    // sas - rodzaj sąsiedzctwa(liczba sąsiadów branych pod uwagę(4 lub 8))
+    fscanf(*config, sas, wys, szer);
+
+    // tutaj tworzymy planszę struktur wys x szer
+
+    scanner(wspolrzedne);
+
+    howmany(dane **plansza, sas);
+
+    change(dane **plansza, sas);
+
+    result (wynik, dane **plansza);
+
+    return 0;
 }
---
+++ struktura.h
@@ -0,0 +1,8 @@
+//
+// Created by Damian on 2017-03-27.
+//
+
+struct {
+    int stan;
+    int liczba_sasiadow;
+}dane;

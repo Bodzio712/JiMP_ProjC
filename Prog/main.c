628cd69d17fedae29e69c05c5cd091c3cfb4865d
Piotr <ppogodzinski@vp.pl>
2017-04-01 21:40:16 +0200

    Szkielet ile_sasiadow

--- Prog/main.c
+++ Prog/main.c
@@ -14,16 +14,41 @@ struct struktura {
 	int sasiedzi[SZEROKOSC][WYSOKOSC];
 } plansza;
 
+//Deklaracje funkcji
 void skaner (FILE * wspolrzedne);
+void zapis_stanu (FILE * plik_zapisu);
+void ile_sasiadow (void);
 
 int main (int argc, char ** argv) {
 	FILE *wspolrzedne = fopen(argv[1],"r");
+	FILE *plik_zapisu = fopen(argv[2], "w");
+
 	skaner (wspolrzedne);
+	ile_sasiadow();
+	printf("czekpojnt\n");
+
 	int x,y;
+
+//Kntrola wczytywania
 	for (x=0; x<SZEROKOSC; x++) {
-		for (y=0; y<WYSOKOSC; y++)
-			printf("x=%d, y=%d, status=%d\n", x, y, plansza.status[x][y]);
+		for (y=0; y<WYSOKOSC; y++) {
+			if (plansza.status[x][y] == 1)
+				printf("*");
+			else
+				printf("O");
+		}
+		printf("\n");
 	}
+
+	 for (x=0; x<SZEROKOSC; x++) {
+                for (y=0; y<WYSOKOSC; y++) {
+			printf ("%d ", plansza.sasiedzi[x][y]);
+                }
+                printf("\n");
+        }
+
+
+	zapis_stanu (plik_zapisu);
 }
 
 void skaner (FILE * wspolrzedne) {
@@ -34,3 +59,42 @@ void skaner (FILE * wspolrzedne) {
         }
 }
 
+void zapis_stanu (FILE * plik_zapisu) {
+	int x,y;
+	for (x=0; x<SZEROKOSC; x++) {
+		for (y=0; y<WYSOKOSC; y++) {
+			if (plansza.status[x][y] == 1)
+				fprintf(plik_zapisu ,"%d %d\n", x, y);
+		}
+	}	
+}
+
+void ile_sasiadow (void) {
+	int ilosc = 0;
+	int x, y;
+	for (x=0; x<SZEROKOSC; x++) {
+                for (y=0; y<WYSOKOSC; y++) {
+			if (POZA_PLANSZA ==1) {
+				int sx=x-1, sy=y-1;
+				int licznik=0;
+				while (licznik<9) {
+					if(sx>x+1) {
+						sx=x-1;
+						sy++;
+					}
+					if(sx<0 || sy<0)
+						ilosc++;
+					else {
+						if (plansza.status[sx][sy] == 1)
+							ilosc++;
+					}
+					sx++;
+					licznik++;
+				}
+			} else {
+			}
+		plansza.sasiedzi[x][y] = ilosc-1;
+		ilosc=0;
+                }
+        }
+}
